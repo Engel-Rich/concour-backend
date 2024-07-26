47a8a917700aad7c3af4f64d18d0f04632c78797
engelbert richelieu <engelbertrichelieumbarga@gmail.com>
2024-07-26 05:07:44 +0200

    Gestion du corp

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('', 200);
+            $response->header('Access-Control-Allow-Origin', '*');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
+            return $response;
+        }
+    
+        $response = $next($request);
+    
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
+    
+        return $response;
+        // return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();   
 // });
-Route::prefix('v1')->group(function () {
+
+
+Route::prefix('v1')->middleware(['cors'])-> roup(function () {
     Route::prefix('auth')->group(function () {
         Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
         Route::post('/register', [App\Http\Controllers\UserController::class, 'register']);
