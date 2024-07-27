d5c8145bacd03ed392666baa0eac52105ef4c003
engelbert richelieu <engelbertrichelieumbarga@gmail.com>
2024-07-28 01:51:17 +0200

    gestion des erreru coté chrome

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,21 +15,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->isMethod('OPTIONS')) {
-            $response = response('', 200);
-            $response->header('Access-Control-Allow-Origin', '*');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
-            return $response;
-        }
-    
-        $response = $next($request);
-    
+	$response = $next($request);
+
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');
-    
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization','Accept');
+
         return $response;
-        // return $next($request);
     }
 }
